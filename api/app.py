2f4e5c4cd6a34e9ad3e23c4ee376d2076c7409d6
inia-app <iniadeveloper@gmail.com>
2025-02-27 00:09:32 +0000

    INIT CODE

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from markitdown import MarkItDown
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+import uvicorn
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from handlers.ocr import read
+
+#inicializando classes
+app = FastAPI()
+@app.post('/upload_file')
+async def main(file: UploadFile):
+    extension = file.filename.split(".")[-1]
+    accepted_forms = {"pdf":read, "jpg":read, "png":read}
+    if extension not in accepted_forms.keys():
+        raise HTTPException(status_code=500, detail="Invalid File Input")
+    file_ = await accepted_forms[extension](file.file)
+
+    return JSONResponse(
+        {"content": file_}
+    )
+if __name__ == '__main__':
+    uvicorn.run("app:app", reload=True)
---
+++ api/handlers/ocr.py
@@ -0,0 +1,32 @@
+from markitdown import MarkItDown
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_KEY")) 
+md = MarkItDown(llm_client=client, llm_model='gpt-4o-mini')
+prompt = '''Write detailed description. Extract text and nothing more.'''
+
+
+async def read(file):
+    try:
+        response = md.convert_stream(file)
+        if not response.text_content:
+            response = md.convert_stream(file, llm_prompt = prompt)
+            if not response.text_content:
+                raise Exception
+        return response.text_content
+    except Exception as e:
+        return f"Erro ao realizar OCR: {e}"
+
+
+async def main():
+    path = 'files/153.pdf'
+    # print(path[-3:])
+    with open(path, 'rb') as f:
+        c = await read(f)
+        print(c)
+if __name__ == '__main__':
+     asyncio.run(main())
