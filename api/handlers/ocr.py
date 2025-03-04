fa1c8a665a0af4753d18f3d91b46e5b75aeab787
inia-app <iniadeveloper@gmail.com>
2025-03-04 13:28:12 +0000

    Update Output to HTML

--- api/handlers/ocr.py
+++ api/handlers/ocr.py
@@ -4,21 +4,22 @@ from dotenv import load_dotenv
 import os
 import asyncio
 import requests
-
+from markdown import markdown
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_KEY")) 
 md = MarkItDown(llm_client=client, llm_model='gpt-4o-mini')
-prompt = '''Write detailed description. Extract text and nothing more.'''
+prompt = '''Write detailed description. Extract text and nothing more. write in portuguese'''
 
 
 async def read(file):
     try:
         response = md.convert_stream(file)
         if not response.text_content:
-            response = md.convert_stream(file, llm_prompt = prompt)
+            response = md.convert_stream(file, llm_prompt = prompt, prompt= prompt)
             if not response.text_content:
                 raise Exception
-        return response.text_content
+        mk = markdown(response.text_content)
+        return mk
     except Exception as e:
         return f"Erro ao realizar OCR: {e}"
 
@@ -39,7 +40,8 @@ async def read_url(url):
 
 
 async def main(path):
-    c = await read_url(path)
+    with open (path, 'rb') as f: 
+        c = await read(f)
     print(c)
 if __name__ == '__main__':
-     asyncio.run(main('https://temp-files-0.sfo3.cdn.digitaloceanspaces.com/b4292e7e-da9a-4fc6-9412-85b95924f869/page_1.pdf'))
+     asyncio.run(main('files/153.pdf'))
